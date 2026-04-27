0529f6a40a084c37e3190c9cfe6b04ea375cc1fd
Clint Paden <fep2@cdc.gov>
2026-04-27 14:08:00 -0400

    remove target and add to gitignore

--- target/release/deps/swdist-f780252c910ab8ec.d
+++
@@ -1,5 +0,0 @@
-/scicomp/home-pure/fep2/git/swdist/target/release/deps/swdist-f780252c910ab8ec.d: src/main.rs
-
-/scicomp/home-pure/fep2/git/swdist/target/release/deps/swdist-f780252c910ab8ec: src/main.rs
-
-src/main.rs:
--- target/release/swdist.d
+++
@@ -1,1 +0,0 @@
-/scicomp/home-pure/fep2/git/swdist/target/release/swdist: /scicomp/home-pure/fep2/git/swdist/src/main.rs
